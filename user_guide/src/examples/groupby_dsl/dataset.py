ed4c708b3b4cdd1ae7f2ca7deb29bd25cc2ea8fc
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2023-09-22 19:52:52 +0800

    renew user_guide.examples

--- user_guide/src/examples/groupby_dsl/dataset.py
+++ user_guide/src/examples/groupby_dsl/dataset.py
@@ -10,6 +10,6 @@ dtypes = {
     "party": pl.Categorical,
 }
 
-dataset = pl.read_csv(url, dtypes=dtypes).with_column(
+dataset = pl.read_csv(url, dtypes=dtypes).with_columns(
     pl.col("birthday").str.strptime(pl.Date, strict=False)
 )  # 使用str命名空间
