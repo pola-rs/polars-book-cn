9db0afde85d8e05efd138817f2c99753a1dc5a54
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2023-09-22 18:32:32 +0800

    update examples

--- user_guide/src/examples/time_series/dynamic_ds.py
+++ user_guide/src/examples/time_series/dynamic_ds.py
@@ -8,6 +8,7 @@ df = pl.DataFrame(
             start=datetime(2021, 12, 16),
             end=datetime(2021, 12, 16, 3),
             interval="30m",
+            eager=True,
         ),
         "groups": ["a", "a", "a", "b", "b", "a", "a"],
     }
