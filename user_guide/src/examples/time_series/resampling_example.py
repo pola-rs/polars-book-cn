542cd4d98a60c90ddeb198a138ddc08f5615a669
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2023-09-22 18:38:38 +0800

    renew examples.time_series

--- user_guide/src/examples/time_series/resampling_example.py
+++ user_guide/src/examples/time_series/resampling_example.py
@@ -4,7 +4,7 @@ import polars as pl
 
 df = pl.DataFrame(
     {
-        "time": pl.date_range(start=datetime(2021, 12, 16), end=datetime(2021, 12, 16, 3), interval="30m"),
+        "time": pl.date_range(start=datetime(2021, 12, 16), end=datetime(2021, 12, 16, 3), interval="30m", eager=True),
         "groups": ["a", "a", "a", "b", "b", "a", "a"],
         "values": [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0],
     }
