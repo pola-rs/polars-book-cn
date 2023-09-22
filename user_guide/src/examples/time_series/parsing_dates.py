6fba28c429e777c0160c6c481deed06cad585c14
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2023-09-22 18:57:11 +0800

    renew examples.time_series

--- user_guide/src/examples/time_series/parsing_dates.py
+++ user_guide/src/examples/time_series/parsing_dates.py
@@ -11,8 +11,6 @@ filtered_range_df = df.filter(
     pl.col("Date").is_between(datetime(1995, 7, 1), datetime(1995, 11, 1)),
 )
 
-annual_average_df = df.groupby_dynamic("Date", every="1y").agg(pl.col("Close").mean())
-
 df_with_year = df.with_column(pl.col("Date").dt.year().alias("year"))
 
 ts = pl.Series(["-1300-05-23", "-1400-03-02"]).str.strptime(pl.Date)
