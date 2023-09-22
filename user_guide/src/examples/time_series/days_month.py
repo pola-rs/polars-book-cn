58839bab4b163e5f03867511bf0a81ff26c5216c
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2023-09-22 17:48:03 +0800

    fixed bugs

--- user_guide/src/examples/time_series/days_month.py
+++ user_guide/src/examples/time_series/days_month.py
@@ -1,8 +1,8 @@
 import polars as pl
-from datetime import date
+from datetime import datetime
 
 # 时间轴（从low到high，间隔为1天，轴名称为"time"）
-df = pl.date_range(low=date(2021, 1, 1), high=date(2021, 12, 31), interval="1d", name="time").to_frame()
+df = pl.date_range(start=datetime(2021, 1, 1), end=datetime(2021, 12, 31), interval="1d", name="time", eager=True).to_frame()
 
 out = (
     df.groupby_dynamic("time", every="1mo", period="1mo", closed="left")
