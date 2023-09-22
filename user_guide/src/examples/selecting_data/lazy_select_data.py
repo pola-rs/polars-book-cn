7a814de9a03926b75fe920c49c44c887e78f01d6
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2023-09-22 17:14:13 +0800

    update user_guide

--- user_guide/src/examples/selecting_data/lazy_select_data.py
+++ user_guide/src/examples/selecting_data/lazy_select_data.py
@@ -5,12 +5,3 @@ lazy_select_df = pl.scan_csv("data/appleStock.csv").select(["Date"])
 
 lazy_select_df = lazy_select_df.explain()
 
-lazy_filter_df = (
-    pl.scan_csv("data/appleStock.csv")
-    .filter(
-        pl.col("Date") == datetime(1995, 10, 16),
-    )
-    .filter(pl.col("Close") > 100)
-)
-
-lazy_filter_df = lazy_filter_df.explain()
