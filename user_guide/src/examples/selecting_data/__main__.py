3e46762f4bcca81a50a7fd4edb4e4cf747054391
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2023-09-22 17:23:02 +0800

    fixed bugs

--- user_guide/src/examples/selecting_data/__main__.py
+++ user_guide/src/examples/selecting_data/__main__.py
@@ -9,7 +9,7 @@ from .indexing_selecting_examples import (
     condition_select_df,
     dtype_select_df,
 )
-from .lazy_select_data import lazy_filter_df, lazy_select_df
+from .lazy_select_data import lazy_select_df
 
 path = create_if_not_exists(f"{OUTPUT_BASE_DIR}/selecting_data")
 
@@ -22,7 +22,6 @@ variable_filename_tuples = [
     (list_select_df, "list_select_df"),
     (condition_select_df, "condition_select_df"),
     (dtype_select_df, "dtype_select_df"),
-    (lazy_filter_df, "lazy_filter_df"),
     (lazy_select_df, "lazy_select_df"),
 ]
 
